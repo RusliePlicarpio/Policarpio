9cf725424a0dc9c66e70d4ead39a2b083722df9d
RusliePlicarpio <rusliepolicarpio1221@gmail.com>
2024-08-23 10:41:13 +0800

    Add files via upload

---
+++ main2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+
+int main() {
+    int choice;
+
+ {
+        std::cout << "\nDirectory Management System\n";
+        std::cout << "1. List Files\n";
+        std::cout << "2. Create Directory\n";
+        std::cout << "3. Change Directory\n";
+        std::cout << "4. Exit\n";
+        std::cout << "Enter your choice: ";
+        std::cin >> choice;
+
+
